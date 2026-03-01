69c710f4de3b83dd4b007cf3e95556ff516ecf0d
Szopus <150285589+Szopus@users.noreply.github.com>
2026-03-01 17:07:27 +0000

    Complete Maven project setup

--- my-java-app/src/main/java/com/example/main.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class Main{
-    public static void main(string[] args) {
-        System.out.println("Hello, Java");
-        System.out.println("Welcome to Maven!");
-    }
-}
